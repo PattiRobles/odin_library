f7405808623e3b3266bc80e95134013013fb170b
Patricia Robles <122781084+PattiRobles@users.noreply.github.com>
2025-01-07 15:22:51 +0000

    improves overlay/card

--- script.js
+++ script.js
@@ -41,13 +41,13 @@ function insertEntry (book, index) {
   
   let overlay = document.createElement('div');
   overlay.setAttribute('id', `overlay${index + 1}`)
-  overlay.classList.add('overlay', 'hidden');
+  overlay.classList.add('overlay');
   overlay.textContent = 'overlay';
   libraryListing.appendChild(overlay)
 
   let bookCard = document.createElement('div');
   bookCard.setAttribute('id', `bookCard${index + 1}`)
-  bookCard.classList.add('bookCard', 'hidden');
+  bookCard.classList.add('bookCard');
   bookCard.textContent = 'card';
   libraryListing.appendChild(bookCard)
 
@@ -56,15 +56,41 @@ function insertEntry (book, index) {
 
 //open book card
 function openBookCard(index) {
- const overlay = document.getElementById(`overlay${index + 1}`);
+  const overlay = document.getElementById(`overlay${index + 1}`);
   const bookCard = document.getElementById(`bookCard${index + 1}`);
-  overlay.classList.toggle('hidden');
-  bookCard.classList.toggle('hidden');
-  bookCard.style.opacity = 
-  bookCard.style.opacity === "1"? "0": "1";
+  overlay.style.display = 'block';
+  bookCard.style.display = 'block';
+
+  //fade in overlay and card
+  setTimeout(() => {
+    overlay.style.opacity = 1;
+    bookCard.style.opacity = 1;
+  }, 10) //delay to allow for changes
 }
 
-//add event listener to close card
+//close book card
+function closeBookCard(index) {
+  const overlay = document.getElementById(`overlay${index + 1}`);
+  const bookCard = document.getElementById(`bookCard${index + 1}`);
+  overlay.style.opacity = 0;
+  bookCard.style.opacity = 0;
+  //why not display none??
+
+  //delay to allow fade and then hide
+  setTimeout(() => {
+    overlay.style.display = 'none';
+    bookCard.style.display = 'none';
+  }, 200) //delay to allow for changes
+
+}
+
+document.querySelectorAll('.overlay').forEach((overlay, index) => {
+  overlay.addEventListener('click', () => closeBookCard(index));
+});
+ 
+
+
 //populate card
 //fade rest of screen - cant manage to fix overlay
 
+//REMOVE HIDDEN CLASS TO SIMPLIFY AND MANAGE DISPLAY OF CARD WITH OPACITY AND DISPLAY
