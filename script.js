696b6d1b359748957030fdc8411711f039aca91b
Patricia Robles <122781084+PattiRobles@users.noreply.github.com>
2025-01-07 14:44:32 +0000

    creates popup card for library items

--- script.js
+++ script.js
@@ -27,16 +27,44 @@ console.log(myLibrary)
 const libraryListing = document.querySelector('#list-container');
 
 //loop to display library content
-myLibrary.forEach((book) => 
-   insertEntry(book.title))
+//INDEX available in forEach needs to be passed as parameter to 1) create unique IDs 2) avoid scoping issues
+myLibrary.forEach((book, index) => 
+   insertEntry(book, index))
   
 
 //function to add array books into screen
-function insertEntry (book) {
-  let libraryEntry = document.createElement("div");
-  libraryEntry.textContent = book;
+function insertEntry (book, index) {
+  let libraryEntry = document.createElement('div');
+  libraryEntry.classList.add('new-entry');
+  libraryEntry.textContent = book.title;
   libraryListing.appendChild(libraryEntry)
+  
+  let overlay = document.createElement('div');
+  overlay.setAttribute('id', `overlay${index + 1}`)
+  overlay.classList.add('overlay', 'hidden');
+  overlay.textContent = 'overlay';
+  libraryListing.appendChild(overlay)
+
+  let bookCard = document.createElement('div');
+  bookCard.setAttribute('id', `bookCard${index + 1}`)
+  bookCard.classList.add('bookCard', 'hidden');
+  bookCard.textContent = 'card';
+  libraryListing.appendChild(bookCard)
+
+  libraryEntry.addEventListener('click', () => openBookCard(index))
 }
 
+//open book card
+function openBookCard(index) {
+ const overlay = document.getElementById(`overlay${index + 1}`);
+  const bookCard = document.getElementById(`bookCard${index + 1}`);
+  overlay.classList.toggle('hidden');
+  bookCard.classList.toggle('hidden');
+  bookCard.style.opacity = 
+  bookCard.style.opacity === "1"? "0": "1";
+}
 
+//add event listener to close card
+//populate card
+//fade rest of screen - cant manage to fix overlay
 
